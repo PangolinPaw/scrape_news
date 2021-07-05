6ecc35ce0304c86bb67b23eca77c073c7c1ea132
Gareth <pangolinpaw@hotmail.co.uk>
2021-07-05 10:45:10 +0100

    Add ability to repeat search at the specified intervals

--- scrape.py
+++ scrape.py
@@ -44,25 +44,27 @@ def get_html(url):
 
 def main(url, search):
 	soup = get_html(url)
+	saved_results = 0
 	if soup:
-		rows = soup.find_all('tr', class_='Periodicals')
+		rows = soup.find_all('tr', class_='news')
 		for row in rows:
 			if row != ' ' and row != None:
 				headline = row.find('div', class_='story_header')
 				if headline != None:
 					headlineText = headline.find('span').text.strip()
 					if search.upper() in headlineText.upper():
-						print(' Matching article found:')
-						print(f'   "{headlineText}"')
+						print(f' [+] Matching article found: "{headlineText}"')
 
 						datestamp, timestamp = row['data-datenews'].split()
 						article_url = headline.find('a', class_='newsTitleLink')['href']
 						ticker = row.find('span', class_='ticker').next_element
 						if not in_history(article_url):
-							print(' Novel article, saving to file')
+							print('     Novel article, saving to file')
 							save(datestamp, ticker, article_url)
+							saved_results += 1
 						else:
-							print(' Article encountered before, ignoring')
+							print('     Article encountered before, ignoring')
+		print( f' [i] {saved_results} articles saved.')
 
 	else:
 		print(f' [!] Error 01: unable to read data from {url}')
@@ -72,7 +74,28 @@ if __name__ == '__main__':
 	if len(arguments) > 1:
 		url = 'https://thefly.com/news.php'
 		search = arguments[1]
-		main(url, search)
+		if len(arguments) > 2:
+			interval = int(arguments[2])
+			print(f' [Repeating search every {interval} minutes]')
+			run_num = 0
+			while True:
+				run_num += 1
+				print(f' [i] Run {run_num}')
+				main(url, search)
+				for x in range(interval * 60):
+					print(f'\r [Next run in {(interval * 60) - (x + 1)} seconds]  ', end='')
+					time.sleep(1)
+				print()
+				print()
+		else:
+			print(' [Executing single search]')
+			main(url, search)
 
 	else:
-		print(' Usage:\n    python scrape.py "phrase to find"')
+		print('''
+ Usage:
+   python scrape.py "<search term>" [interval]
+
+   search term			A word or phrase which articles must contain.
+   interval			(Optional) The number of minutes between each repetition
+ 				of the search. If blank, seach is completed only once.''')
